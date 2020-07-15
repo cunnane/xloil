642820f8bf8a1b61e263bc0c6ad7e59bb9370a2d
SteveC <cunnane@gmail.com>
2020-07-15 17:29:09 +0200

    Include comdef.h in header file - fixes build break.

--- src/COMInterface/ExcelTypeLib.h
+++ src/COMInterface/ExcelTypeLib.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <oleacc.h> // must include this before type imports
+#include <comdef.h>
 
 // MSO.dll
 #import "libid:2DF8D04C-5BFA-101B-BDE5-00AA0044DE52" \
@@ -25,3 +26,10 @@ using namespace VBIDE;
 
 extern "C" const GUID __declspec(selectany) LIBID_Excel =
   { 0x00020813,0x0000,0x0000,{0xc0,0x00,0x00,0x00,0x00,0x00,0x00,0x46} };
+
+
+#define XLO_RETHROW_COM_ERROR \
+  catch (_com_error& error)\
+  {\
+    XLO_THROW(L"COM Error {0:#x}: {1}", (size_t)error.Error(), error.ErrorMessage());\
+  }
