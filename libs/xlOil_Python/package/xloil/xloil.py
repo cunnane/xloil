dceaf55715131cd2d1e999f580680396131b9124
SteveC <cunnane@gmail.com>
2020-09-15 23:49:30 +0200

    Fix missing comma

--- libs/xlOil_Python/package/xloil/xloil.py
+++ libs/xlOil_Python/package/xloil/xloil.py
@@ -21,7 +21,7 @@ if importlib.util.find_spec("xloil_core") is not None:
         CellError, FuncOpts, Range, ExcelArray, in_wizard, log,
         event, cache, RtdServer, RtdPublisher, get_event_loop,
         register_functions, deregister_functions,
-        create_ribbon, run_later
+        create_ribbon, run_later,
         CannotConvert, set_return_converter,
         CustomConverter as _CustomConverter,
         CustomReturn as _CustomReturn)
