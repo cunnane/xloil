c94e4fa7528aded7ab3d9740dc8d3d9e72544c2b
SteveC <cunnane@gmail.com>
2021-01-19 11:31:17 +0000

    Add COM interface usage to static test addin.

---
+++ tests/TestAddin/CallbackTest.cpp
@@ -0,0 +1,28 @@
+#include <xloil/ExcelTypeLib.h>
+#include <xloil/StaticRegister.h>
+#include <xloil/ExcelCall.h>
+#include <xloil/ExcelObj.h>
+#include <xloil/ExcelRef.h>
+#include <xloil/Date.h>
+#include <xloil/ExcelApp.h>
+
+using std::shared_ptr;
+
+namespace xloil
+{
+  // Replicates the TODAY() function by changing the format of the calling
+  // cell to date
+  XLO_FUNC_START(testToday())
+  {
+    CallerInfo caller;
+    excelPost([=] 
+    {
+      excelApp().Range[caller.writeAddress().c_str()]->NumberFormat = L"dd-mm-yyyy"; 
+    });
+    std::tm buf; 
+    auto now = std::time(0);
+    localtime_s(&buf, &now);
+    return returnValue(buf);
+  }
+  XLO_FUNC_END(testToday);
+}
