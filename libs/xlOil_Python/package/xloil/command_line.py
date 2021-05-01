f1e0d0c310255651f78f00a0a11ab7e47655753f
SteveC <cunnane@gmail.com>
2021-05-02 00:50:42 +0100

    Rewrite python installer script in pure python to avoid dependence on powershell and the need to enable scripts.  Also specify encoding in pathlib.read_text to fix issue #17

--- libs/xlOil_Python/package/xloil/command_line.py
+++ libs/xlOil_Python/package/xloil/command_line.py
@@ -1,68 +1,176 @@
-import subprocess
 import sys
 import os
 from pathlib import Path
 import re
+import winreg as reg
+from contextlib import suppress
+import binascii
+import shutil as sh
 
-_powershellMessage = "Failed to execute script. You may need to change powershell permissions " + \
-                     "by typing 'powershell -Command Set-ExecutionPolicy -Scope CurrentUser RemoteSigned'"
+_ADDIN_NAME    = "xlOil.xll"
+_INIFILE_NAME  = "xlOil.ini"
+_APP_DATA_PATH = Path(os.getenv('APPDATA'))
+_XL_START_PATH = _APP_DATA_PATH / "Microsoft" / "Excel" / "XLSTART"
+_XLOIL_BIN_DIR = Path(sys.prefix) / "share" / "xloil"
 
-def _runPowerShell(script):
-    result = subprocess.run(f"powershell {script}", shell=True)
-    if result.returncode != 0:
-        raise Exception(_powershellMessage)
 
-def _script_dir():
-    return os.path.join(sys.prefix, "share", "xloil")
+def _excel_regpath(version):
+    return rf"Software\Microsoft\Office\{version}\Excel"
 
-def _install_xloil():
-    target_script = os.path.join(_script_dir(), "xloil_Install.ps1")
-    _runPowerShell(target_script)
+def _find_excel_version():
+    # Will fetch a value like "Excel.Application.16"
+    # Note for Office 2019, it still gives version 16...hmm
+    verstr = reg.QueryValue(reg.HKEY_CLASSES_ROOT, r"Excel.Application\CurVer")
+    return int(verstr.replace('Excel.Application.',''))
 
-    # Edit the xloil.ini file. To preserve comments and whitespace it's easier to just do 
-    # a regex replace rather than read the file as structured TOML
+def _check_VBA_access(version):
 
-    ini_path = Path(os.getenv('APPDATA')) / "xlOil" / "xlOil.ini"
-    if not ini_path.exists():
-        raise FileNotFoundError(f"Could not find {ini_path}. xloil_Install.ps1 may not have run - check permissions")
+    user_access = 1
+    lm_access = 1
 
-    ini_txt = ini_path.read_text()
-    
+    with suppress(OSError): 
+        user_key = reg.OpenKey(reg.HKEY_CURRENT_USER, _excel_regpath(version) + r"\Security")
+        user_access, = reg.QueryValueEx(user_key, "AccessVBOM")
+    with suppress(OSError):
+        lm_key =  reg.OpenKey(reg.HKEY_LOCAL_MACHINE, _excel_regpath(version) + r"\Security")
+        lm_access, = reg.QueryValueEx(lm_key, "AccessVBOM")
+
+    if user_access == 0 or lm_access == 0:
+        print("To ensure xlOil local functions work, allow access to the VBA Object Model in\n" +
+			"Excel > File > Options > Trust Center > Trust Center Settings > Macro Settings\n")
+
+def _remove_from_resiliancy(filename, version):
+
+    # Source https://stackoverflow.com/questions/751048/how-to-programatically-re-enable-documents-in-the-ms-office-list-of-disabled-fil
+
+    #Converts the File Name string to UTF16 Hex
+    filename_hex = binascii.hexlify(filename.encode('utf-16'))
+
+    # If we can't find the key or exit the for loop, suppress the error
+    with suppress(OSError): 
+        regkey = reg.OpenKey(reg.HKEY_CURRENT_USER, _excel_regpath(version) + "\Resiliency\DisabledItems")
+
+        for i in range(1024):
+            name, value, = reg.EnumValue(regkey, i)
+            value_hex = binascii.hexlify(value.encode('utf-16'))
+            if filename_hex in value:
+                reg.DeleteValue(regkey, name)
+
+def _remove_addin(version):
+
+    # If we can't find the key or exit the for loop, suppress the error
+    with suppress(OSError): 
+        regkey = reg.OpenKey(reg.HKEY_CURRENT_USER, _excel_regpath(version) + "\Add-in Manager")
+
+        addin_path = _XL_START_PATH / _ADDIN_NAME
+        # Cycles through all the properties and delete if it contains the file name.
+        for i in range(1024):
+            name, value, = reg.EnumValue(regkey, i)
+            if addin_path in value:
+                reg.DeleteValue(regkey, name)
+
+
+
+def _toml_lit_string(s):
+    # TOML literal strings have a lot of quotes and escapes, this function does the encoding
+    return "'''" + s.replace('\\','\\\\') + "'''"
+
+def _write_python_path_to_ini(ini_txt):
+
     python_path = os.path.join(sys.prefix, "Lib") + ";" + os.path.join(sys.prefix, "DLLs") 
     python_ver = f'{sys.version_info.major}.{sys.version_info.minor}'
-
-    def toml_lit_string(s):
-        return "'''" + s.replace('\\','\\\\') + "'''"
-
-    ini_txt, count1 = re.subn(r'^(\s*PYTHONPATH\s*=).*', r'\g<1>' + toml_lit_string(python_path), ini_txt, flags=re.M)
-    ini_txt, count2 = re.subn(r'^(\s*PYTHON_LIB\s*=).*', r'\g<1>' + toml_lit_string(sys.prefix), ini_txt, flags=re.M)
+    
+    # Set PYTHONPATH
+    ini_txt, count1 = re.subn(r'^(\s*PYTHONPATH\s*=).*', r'\g<1>' + _toml_lit_string(python_path), ini_txt, flags=re.M)
+    # Set PYTHON_LIB
+    ini_txt, count2 = re.subn(r'^(\s*PYTHON_LIB\s*=).*', r'\g<1>' + _toml_lit_string(sys.prefix), ini_txt, flags=re.M)
+    # Set xlOilPythonVersion
     ini_txt, count3 = re.subn(r'^(\s*xlOilPythonVersion\s*=).*', rf'\g<1>"{python_ver}"', ini_txt, flags=re.M)
+    # Set XLOIL_PATH
+    ini_txt, count4 = re.subn(r'^(\s*XLOIL_PATH\s*=).*', r'\g<1>' + _toml_lit_string(str(_XLOIL_BIN_DIR)), ini_txt, flags=re.M)
+ 
+    # Check if any of the counts is not 1, i.e. the expression matched zero or multiple times    
+    return ini_txt, count1 == 1 and count2 == 1 and count3 == 1 and count4 == 1
+    
+   
+def _install_xloil():
 
-    if count1 != 1 or count2 != 1 or count3 != 1:
+    ini_path = _APP_DATA_PATH / "xlOil" / _INIFILE_NAME
+
+    excel_version = _find_excel_version()
+
+    # Just in case we got put in Excel's naughty corner for misbehaving addins
+    _remove_from_resiliancy(_ADDIN_NAME, excel_version)
+
+    # Check access to the VBA Object model (for local functions)
+    _check_VBA_access(excel_version)
+
+    # Ensure XLSTART dir really exists
+    with suppress(FileExistsError):
+        os.mkdir(_XL_START_PATH)
+
+    # Copy the XLL
+    sh.copy(_XLOIL_BIN_DIR / _ADDIN_NAME, _XL_START_PATH)
+    print("Installed ", _XL_START_PATH / _ADDIN_NAME)
+    
+    # Copy the ini file to APPDATA, avoiding overwritting any existing ini
+    if ini_path.exists():
+        print("Found existing settings file at \n", ini_path)
+    else:
+        with suppress(FileExistsError):
+            ini_path.parent.mkdir()
+        sh.copy(_XLOIL_BIN_DIR / _INIFILE_NAME, ini_path)
+
+    # Edit the xloil.ini file. To preserve comments and whitespace it's easier to just use
+    # regex replace rather than read the file as structured TOML
+    ini_txt = ini_path.read_text(encoding='utf-8')
+
+    ini_txt, success = _write_python_path_to_ini(ini_txt)
+    
+    # Check if any of the counts is not 1, i.e. the expression matched zero or multiple times
+    if not success:
         print(f'WARNING: Failed to set python paths in {ini_path}. You may have to do this manually.')
     else:
         ini_path.write_text(ini_txt)
         print(f'Edited {ini_path} to point to {sys.prefix} python distribution.')
 
 def _remove_xloil():
-    target_script = os.path.join(_script_dir(), "xloil_Remove.ps1")
-    _runPowerShell(target_script)
+
+    excel_version = _find_excel_version()
+    
+    # Ensure no xlOil addins are in the registry
+    _remove_addin(excel_version)
+    
+    os.remove(_XL_START_PATH / _ADDIN_NAME)
+
 
 def _create_addin(args):
     if len(args) != 1:
         raise Exception("'create' should have one argument, the target filename")
 
     filename = args[0]
+    basename = Path(os.path.splitext(filename)[0])
 
-    target_script = os.path.join(_script_dir(), "xloil_NewAddin.ps1")
-    _runPowerShell(f'{target_script} {filename}')
+    xll_path = basename.with_suffix(".xll")
+    ini_path = basename.with_suffix(".ini")
 
-    ini_path = Path(filename.replace('xll', 'ini'))
-    ini_txt = ini_path.read_text()
+    sh.copy(_XLOIL_BIN_DIR / _ADDIN_NAME, xll_path)
+    sh.copy(_XLOIL_BIN_DIR / "NewAddin.ini",  ini_path)
+    
+    print("New addin created at: ", xll_path)
 
+    # Edit ini file
+    ini_txt = ini_path.read_text(encoding='utf-8')
+    
+    # Assume we want the xlOil_Python plugin as we're running a python script
     ini_txt, count = re.subn(r'^(\s*Plugins\s*=).*', r'\g<1>["xlOil_Python"]', ini_txt, flags=re.M)
-
+    
+    # Assume we want the python paths set to the distribution running this script
+    ini_txt, success = _write_python_path_to_ini(ini_txt)
+    
     ini_path.write_text(ini_txt)
+
+    print("xlOil_Python plugin enabled using python installed at: ", sys.prefix)
 
 
 def main():
