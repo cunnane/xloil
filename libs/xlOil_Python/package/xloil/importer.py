d4fb9f352fc6d1866086a6c98ccaf4e35067ce82
SteveC <cunnane@gmail.com>
2021-12-21 18:33:51 +0000

    Python: implement import hooks using the more modern sys.path_hooks rather than replacing the import statement.  Make linked_workbook and source_addin use filenames for lookup as this is readily available in the stack tracesaves figuring out the module name

--- libs/xlOil_Python/package/xloil/importer.py
+++ libs/xlOil_Python/package/xloil/importer.py
@@ -12,33 +12,35 @@ import inspect
 
 from .register import scan_module, _clear_pending_registrations
 from ._core import StatusBar
-from ._common import log_except
+from ._common import log, log_except
 
-_module_addin_map = dict()
+_module_addin_map = dict() # Keeps track of which addin loads a particular source file
 _linked_workbooks = dict()
 
-class _ModuleFinder(importlib.abc.MetaPathFinder):
-
+class _SpecifiedPathFinder(importlib.abc.MetaPathFinder):
     """
-    Allows importing a module from a path specified in path_map
-    without needing to add it to sys.paths - essentially a private
-    set of import paths, indexed by module name
+    Allows importing a module from a path specified in path_map without
+    needing to add it to sys.paths - essentially a private set of import 
+    paths, indexed by module name
     """
 
-    path_map = dict()
+    _path_map = dict()
 
     def find_spec(self, fullname, path, target=None):
-        path = self.path_map.get(fullname, None)
+        path = self._path_map.get(fullname, None)
         if path is None:
             return None
-        return importlib.util.spec_from_file_location(fullname, self.path_map[fullname])
+        return importlib.util.spec_from_file_location(fullname, path)
 
     def find_module(self, fullname, path):
         return None
 
-# We maintain a _ModuleFinder on sys.meta_path to catch any reloads of our off-path
-# imported modules loaded by _import_file
-_module_finder = _ModuleFinder()
+    def add_path(self, name, path):
+        self._path_map[name] = path
+
+# Install a sys.meta_path hook. This allows reloads to work for modules 
+# we import from specific path in _import_file
+_module_finder = _SpecifiedPathFinder()
 sys.meta_path.append(_module_finder)
 
 
@@ -49,18 +51,19 @@ def linked_workbook(mod=None):
         If no module is specified, the calling module is used.
     """
     if mod is None:
-        # Get caller
+        # Get immediate caller
         frame = inspect.stack()[1]
-        mod = inspect.getmodule(frame[0])
-    return _linked_workbooks.get(mod.__name__, None)
+    return _linked_workbooks.get(frame.filename, None)
 
 
 def source_addin(mod=None):
     if mod is None:
-        # Get top-level caller
-        frame = inspect.stack()[-1]
-        mod = inspect.getmodule(frame[0])
-    return _module_addin_map.get(mod.__name__, None)
+        # Get the highest level caller we recognise
+        for frame in inspect.stack()[::-1]:
+            addin = _module_addin_map.get(frame.filename, None)
+            if addin is not None:
+                return addin
+    return None
 
 
 def _import_scan(what, addin):
@@ -68,15 +71,15 @@ def _import_scan(what, addin):
     Loads or reloads the specifed module, which can be a string name
     or module object, then calls scan_module.
 
-    Internal use only, users should prefer to import "xloil.importers"
-    which hooks import/reload to trigger a module scan.
+    Internal use only.
     """
     
     if isinstance(what, str):
-        _module_addin_map[what] = addin
         module = importlib.import_module(what)
+        # Remember which addin loaded this module
+        _module_addin_map[module.__file__] = addin ## TODO: hasattr
     elif inspect.ismodule(what):
-        module = importlib.reload(what) # can we avoid calling our hooked reload?
+        module = importlib.reload(what)
     else:
         # We don't care about the return value currently
         result = []
@@ -94,7 +97,7 @@ def _import_file(path, addin, workbook_name=None):
     """
     Imports the specifed py file as a module without adding its path to sys.modules.
 
-    Optionally also adds xlOil linked workbook name information.
+    Optionally also adds xlOil linked workbook information.
     """
 
     with StatusBar(3000) as status:
@@ -107,12 +110,12 @@ def _import_file(path, addin, workbook_name=None):
             module_name = filename
             if workbook_name is not None:
                 module_name = "xloil_wb_" + filename
-                _linked_workbooks[module_name] = workbook_name
+                _linked_workbooks[path] = workbook_name
 
             if len(directory) > 0 or workbook_name is not None:
-                _module_finder.path_map[module_name] = path
+                _module_finder.add_path(module_name, path)
 
-            _module_addin_map[module_name] = addin
+            _module_addin_map[path] = addin
             module = importlib.import_module(module_name)
 
             # Calling import_module will bypass our import hook, so scan_module explicitly
@@ -128,33 +131,26 @@ def _import_file(path, addin, workbook_name=None):
             status.msg(f"Error loading {path}, see log")
 
 
-_real_builtin_import   = builtins.__import__
-_real_importlib_reload = importlib.reload
+def _install_hook():
+    # Hooks the import mechanism to run register.scan_module on all .py files.
+    # We copy _bootstrap_external._install, replacing the source loader with one which 
+    # runs scan_module and install our finder at the start of sys.path_hooks
 
+    from importlib.machinery import (
+        SOURCE_SUFFIXES, BYTECODE_SUFFIXES, FileFinder, 
+        ExtensionFileLoader, SourcelessFileLoader, SourceFileLoader
+        )
+    import _imp
 
+    class LoadAndScan(SourceFileLoader):
+        def exec_module(self, module):
+            super().exec_module(module)
+            scan_module(module)
 
-def _import_hook(name, *args, **kwargs):
+    extensions = ExtensionFileLoader, _imp.extension_suffixes()
+    source     = LoadAndScan, SOURCE_SUFFIXES
+    bytecode   = SourcelessFileLoader, BYTECODE_SUFFIXES
 
-    #TODO: check if already in sys.modules
+    sys.path_hooks.insert(0, FileFinder.path_hook(*[extensions, source, bytecode]))
 
-    module = _real_builtin_import(name, *args, **kwargs)
-
-    # If name is of the form "foo.bar", the result of _real_builtin_import will point to 
-    # the top level package, not the module we want to scan
-    real_module = sys.modules.get(name, module)
-    scan_module(real_module)
-
-    return module
-
-def _reload_hook(*args, **kwargs):
-    _clear_pending_registrations(args[0])
-    module = _real_importlib_reload(*args, **kwargs)
-    scan_module(module)
-    return module
-
-builtins.__import__ = _import_hook
-importlib.reload = _reload_hook
-
-def _unhook_import():
-    builtins.__import__ = _real_builtin_import
-    importlib.reload    = _real_importlib_reload
+_install_hook()
