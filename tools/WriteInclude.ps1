963f5ff3ccec838bc0401bdfd0481ff48e414903
SteveC <cunnane@gmail.com>
2022-08-11 22:36:32 +0200

    Ensure header file is created in WriteIncludes if it doesn't already exist.

--- tools/WriteInclude.ps1
+++ tools/WriteInclude.ps1
@@ -2,8 +2,8 @@
 $HeaderDir = $args[0]
 $TargetDir = $args[1]
 
-
 $xloilHeader = Join-Path $TargetDir "xlOil.h"
+New-Item $xloilHeader
 Set-Content -Path $xloilHeader -Value "#pragma once"
 
 Get-ChildItem -Path $HeaderDir -File -Filter *.h -Name | 
