97b4b3bbcaefe32184b9e9ebd21cef35ca4ae7ce
SteveC <cunnane@gmail.com>
2023-01-30 23:52:38 +0000

    Add log file collision detection and resolution in case more than one Excel instance is opened.

--- src/xlOil-XLL/Log.cpp
+++ src/xlOil-XLL/Log.cpp
@@ -8,10 +8,12 @@
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/msvc_sink.h>
 #include <spdlog/sinks/rotating_file_sink.h>
+#include <filesystem>
 
 using std::wstring;
 using std::string;
 using std::make_shared;
+namespace fs = std::filesystem;
 
 namespace xloil
 {
@@ -68,8 +70,22 @@ namespace xloil
     const std::wstring_view& logFilePath, const char* logLevel,
     const size_t maxFileSizeKb, const size_t numFiles)
   {
+    auto filename = wstring(logFilePath);
+
+    // Open for exclusive acces to check if another Excel instance is using the log file
+    auto handle = CreateFile(
+      filename.c_str(), 
+      GENERIC_READ, 
+      0, // no sharing, exclusive
+      NULL, OPEN_EXISTING, 0, NULL);
+
+    if ((handle != NULL) && (handle != INVALID_HANDLE_VALUE))
+      CloseHandle(handle);
+    else
+      filename = fs::path(filename).replace_extension(std::to_wstring(GetCurrentThreadId()) + L".log");
+
     auto fileWrite = make_shared<spdlog::sinks::rotating_file_sink_mt>(
-      wstring(logFilePath), maxFileSizeKb * 1024, numFiles);
+      filename, maxFileSizeKb * 1024, numFiles);
     fileWrite->set_level(spdlog::level::from_str(logLevel));
     logger->sinks().push_back(fileWrite);
     if (fileWrite->level() < logger->level())
