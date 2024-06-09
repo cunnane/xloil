53f47f52a4daa24976bbb1c71faf47f43f98dcea
Steven <cunnane@gmail.com>
2024-06-09 22:02:37 +0200

    Remove duplicate classifier in setup.py

--- libs/xlOil_Python/Package/setup.py
+++ libs/xlOil_Python/Package/setup.py
@@ -99,7 +99,6 @@ setup(
     
     classifiers=[
         'Development Status :: 4 - Beta',
-        "Programming Language :: Python :: 3",
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
