778109260b9c772ba6b01fc05bffe2dbc2dd852b
Steven <cunnane@gmail.com>
2025-01-26 23:30:33 +0100

    Add xlOil_RegistryClean script to remove any xlOil registry entries.
    (cherry picked from commit 8581fe905fd0ded0eacf0da273584168f0c41831)

---
+++ tools/xlOil_RegistryClean.ps1
@@ -0,0 +1,16 @@
+
+$XloilClassIDs = (Get-ChildItem "Registry::HKEY_CLASSES_ROOT\CLSID\*\InprocServer32") | Where-Object {$_.GetValue("") -like "*xlOil.dll"} | ForEach-Object {$_."PSParentPath"}
+$XloilClasses = (Get-ChildItem "Registry::HKEY_CLASSES_ROOT") | Where-Object {$_.Name -like "*xlOil*"} | ForEach-Object {$_."PSPath"}
+$XloilAddins = (Get-ChildItem HKCU:\Software\Microsoft\Office\Excel\AddIns)  | Where-Object {$_.Name -like "*xloil*"} | ForEach-Object {$_."PSPath"}
+$XloilAddinData = (Get-ChildItem HKCU:\Software\Microsoft\Office\Excel\AddinsData)  | Where-Object {$_.Name -like "*xlOil*"} | ForEach-Object {$_."PSPath"}
+
+$xloilKeys = $XloilClassIDs + $XloilClasses + $XloilAddins + $XloilAddinData
+
+
+Write-Host "=== Found the following xloil keys:"
+$xloilKeys -join "`r`n" | Out-String
+
+$confirmation = Read-Host "=== Press Y to delete"
+if ($confirmation -eq 'y') {
+    $xloilKeys | ForEach-Object {Remove-Item $_ -Recurse}
+}
