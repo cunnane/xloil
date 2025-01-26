204d75a09c92f1b8e56037e71175c6d66877ee24
andyvandy <vandenhoeven.andrew@gmail.com>
2025-01-26 15:40:49 -0500

    Add handling for integer overflow when using implicit type xlfromPy return conversion

--- libs/xlOil_Python/TypeConversion/BasicTypes.h
+++ libs/xlOil_Python/TypeConversion/BasicTypes.h
@@ -403,7 +403,13 @@ namespace xloil
         }
         else if (PyLong_Check(p))
         {
-          return ExcelObj(PyLong_AsLong(p));
+          int overflowIndicator;
+          auto out = PyLong_AsLongAndOverflow(p, &overflowIndicator);
+          if (overflowIndicator)
+            return ExcelObj(PyFloat_AsDouble(p));
+          else
+            return ExcelObj(out);
+
         }
         else if (PyFloat_Check(p))
         {
