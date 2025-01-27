4e5847bbd8ad2cf2d1bc0bf6cc93c702021d4606
Steven <cunnane@gmail.com>
2025-01-27 01:26:47 +0100

    Add help to xlOil_RegistryClean script
    (cherry picked from commit 22b87bdf7ede36062da75a13310baf83a605e1a3)

--- tools/xlOil_RegistryClean.ps1
+++ tools/xlOil_RegistryClean.ps1
@@ -1,8 +1,24 @@
+<#
+.SYNOPSIS
+Removes all xlOil registry entries
 
-$XloilClassIDs = (Get-ChildItem "Registry::HKEY_CLASSES_ROOT\CLSID\*\InprocServer32") | Where-Object {$_.GetValue("") -like "*xlOil.dll"} | ForEach-Object {$_."PSParentPath"}
-$XloilClasses = (Get-ChildItem "Registry::HKEY_CLASSES_ROOT") | Where-Object {$_.Name -like "*xlOil*"} | ForEach-Object {$_."PSPath"}
-$XloilAddins = (Get-ChildItem HKCU:\Software\Microsoft\Office\Excel\AddIns)  | Where-Object {$_.Name -like "*xloil*"} | ForEach-Object {$_."PSPath"}
-$XloilAddinData = (Get-ChildItem HKCU:\Software\Microsoft\Office\Excel\AddinsData)  | Where-Object {$_.Name -like "*xlOil*"} | ForEach-Object {$_."PSPath"}
+.DESCRIPTION
+xlOil should remove its registry entries on exit, however if Excel does not exit cleanly or other issues
+disrupt the removal, some keys may remain. This script will not remove entries for any custom COM addins.
+#>
+
+$XloilClassIDs = (Get-ChildItem "Registry::HKEY_CLASSES_ROOT\CLSID\*\InprocServer32") `
+    | Where-Object {$_.GetValue("") -like "*xlOil.dll"} `
+    | ForEach-Object {$_."PSParentPath"}
+$XloilClasses = (Get-ChildItem "Registry::HKEY_CLASSES_ROOT") `
+    | Where-Object {$_.Name -like "*xlOil*"} `
+    | ForEach-Object {$_."PSPath"}
+$XloilAddins = (Get-ChildItem HKCU:\Software\Microsoft\Office\Excel\AddIns) `
+    | Where-Object {$_.Name -like "*xloil*"} `
+    | ForEach-Object {$_."PSPath"}
+$XloilAddinData = (Get-ChildItem HKCU:\Software\Microsoft\Office\Excel\AddinsData) `
+    | Where-Object {$_.Name -like "*xlOil*"} `
+    | ForEach-Object {$_."PSPath"}
 
 $xloilKeys = $XloilClassIDs + $XloilClasses + $XloilAddins + $XloilAddinData
 
